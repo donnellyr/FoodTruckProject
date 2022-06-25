4daa1c4718eedfc2a47aaf665512781969511baf
Ryan Donnelly <ryan@Ryans-MBP.home>
2022-06-25 08:26:20 -0600

    Initial working version

--- src/com/skilldistilery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistilery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,86 @@
 package com.skilldistilery.foodtruck.app;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.skilldistilery.foodtruck.entities.Foodtruck;
+
 public class FoodTruckApp {
+	Foodtruck output = new Foodtruck();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FoodTruckApp app = new FoodTruckApp();
+		app.userInput();
+		
+	}
+	private void userInput() {
+		Scanner sc = new Scanner(System.in);
+		int truckID = 0, truckRating =0, average = 0;
+		String truckName ="", foodType ="";
+		String truckList[] = new String[5];
+		int winner[] = new int[5];
+		for(int i = 0; i < truckList.length; i++) {
+			System.out.println("Please enter a food truck name or Quit to continue");
+			truckName = sc.nextLine();
+			if(truckName.equals("Quit") || truckName.equals("quit")) {
+					break;
+				}
+				
+			else if(truckName != null)
+				System.out.println("What type of food does this truck serve?");
+				foodType = sc.nextLine();
+							
+				System.out.println("On a scale of 1-5 what would you rate this truck?");
+				truckRating = sc.nextInt();
+				++truckID;
+				truckList[i] = "Truck ID: " + truckID + " Truck Name: " + truckName + " Food Type: " + foodType + " Rating: " + truckRating;
+				average += truckRating;
+				winner[i] = truckRating;
+				truckName = sc.nextLine();
+				
+				}
 
+		int choice =0;
+		boolean repeat = true;
+		while(repeat){
+			System.out.println("1. List all trucks");
+			System.out.println("2. See truck raiting average");
+			System.out.println("3. Display the highest rated truck");
+			System.out.println("4. quit");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1: output.getfoodTruck(truckList);
+			//appMenue();
+			break;
+			case 2: System.out.println("Average truck Rating: " + output.truckAverage(average, truckID));
+			//appMenue();
+			break;
+			case 3: output.bestTruck(truckList, winner);
+			//appMenue();
+			break;
+			case 4: System.out.println("Have a good day!"); repeat = false;
+			break;
+			
+			
+			}
+		}}
+			
+			//sc.close();
+		//appMenue();
+			
+		
+		
+		
+	
+
+	
+	
+	
+	private void appMenue() {
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
+
+
--- src/com/skilldistilery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistilery/foodtruck/entities/Foodtruck.java
@@ -1,5 +1,41 @@
 package com.skilldistilery.foodtruck.entities;
 
-public class Foodtruck {
+import com.skilldistilery.foodtruck.app.FoodTruckApp;
 
+public class Foodtruck {
+	
+	public void bestTruck(String[] truckList, int[] rating) {
+		int currentBest =0;
+		String bestTruck ="";
+		for (int i = 0; i < rating.length; i++) {
+			if(rating[i] > currentBest) {
+				currentBest = rating[i];
+				bestTruck = truckList[i];
+			}
+		}
+		System.out.println("The best rated truck is " + bestTruck);
+	}
+
+	
+
+
+	public void getfoodTruck(String[] truckList) {
+		
+for(int i = 0; i < truckList.length; i++) {
+	if(truckList[i] == null) {
+		break;
+	}
+System.out.println(truckList[i]);
+
+		}
+	}
+
+
+	public int truckAverage(int average, int truckID) {
+		int output = average / truckID;
+		return output;
+	}
 }
+
+
+
